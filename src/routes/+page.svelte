43ff3404146a5a9cf86085b4d55de54c8e9e310a
darshan <darshanshah230@gmail.com>
2023-03-17 19:44:06 -0400

    simple css addition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,11 +42,69 @@
     reset();
   }
 </script>
-<button on:click={clearMessages}>Clear Messages</button>
-<h1>Messages</h1>
-{#each $messages.filter(message => message.role !== 'system') as message}
-    <div>
-        <p>{message.content}</p>
-    </div>
-{/each}
-<input disabled={chatting} bind:value={newMessage} on:keydown={handleKeydown}/>
+<style>
+  * {
+    font-size: 18px;
+    font-family: 'Roboto', sans-serif;
+  }
+  .container {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .message {
+    border-radius: 10px;
+    padding: 10px;
+    margin: 10px;
+    width: 50%;
+  }
+  .user-message {
+    background-color: #e6e6e6;
+    align-self: flex-end;
+  }
+  .gpt-message {
+    background-color: #6e6e6e;
+    align-self: flex-start;
+  }
+  .chat {
+    flex: auto;
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: column;
+  }
+  .input-container {
+    padding: 5px;
+  }
+  .input-container > input {
+    box-sizing: border-box;
+    display: block;
+    width: 100%;
+    border: 3px solid #000;
+    padding: 10px;
+    color: #000;
+    background: transparent;
+    border-radius: var(--size-radius);
+  }
+  .clear-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background: #f77;
+    color: #000;
+    border: 3px solid #000;
+  }
+</style>
+<div class="container">
+  <button class="clear-button" on:click={clearMessages}>Clear Messages</button>
+  <div class='chat'>
+    {#each $messages.filter(message => message.role !== 'system') as message}
+      <div class={'message ' + (message.role === 'user' ? 'user-message' : 'gpt-message')}>
+        <p style="margin:0">{message.content}</p>
+      </div>
+    {/each}
+  </div>
+  <div class="input-container">
+    <input disabled={chatting} bind:value={newMessage} on:keydown={handleKeydown}/>
+  </div>
+</div>
