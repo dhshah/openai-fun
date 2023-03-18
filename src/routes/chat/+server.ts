f3242138a943cbff8f5de7c794b83c1656c839b3
darshan <darshanshah230@gmail.com>
2023-03-17 20:33:40 -0400

    add token limit to env

--- src/routes/chat/+server.ts
+++ src/routes/chat/+server.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
-import { OPENAPI_API_KEY } from '$env/static/private';
+import { OPENAPI_API_KEY, TOKEN_LIMIT } from '$env/static/private';
 
 import { Configuration, OpenAIApi } from 'openai';
 
@@ -10,6 +10,8 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+const token_limit = TOKEN_LIMIT ? parseInt(TOKEN_LIMIT) : 30;
+
 export const POST = (async ({ request }) => {
 	const { messages } = await request.json();
 
@@ -17,7 +19,7 @@ export const POST = (async ({ request }) => {
 		const chatGPT = await openai.createChatCompletion({
 			model: 'gpt-3.5-turbo',
 			messages,
-			max_tokens: 30
+			max_tokens: token_limit
 		});
 		return json(chatGPT.data.choices[0].message);
 	} catch (e) {
