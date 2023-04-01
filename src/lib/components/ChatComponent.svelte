6511777c2aeb155bf48b5593978d9aedece10271
darshan <darshanshah230@gmail.com>
2023-04-01 02:34:28 -0400

    add search button

--- src/lib/components/ChatComponent.svelte
+++ src/lib/components/ChatComponent.svelte
@@ -8,13 +8,18 @@
 	let inputBox: HTMLInputElement;
 
 	let chatting: boolean;
+
+	async function chat() {
+		chatting = true;
+		let val = (' ' + value).slice(1);
+		value = '';
+		await onTrigger(val);
+		chatting = false;
+	}
+
 	async function handleKeydown(e: any) {
 		if (e.key === 'Enter') {
-			chatting = true;
-			let val = (' ' + value).slice(1);
-			value = '';
-			await onTrigger(val);
-			chatting = false;
+			await chat();
 		}
 	}
 
@@ -28,11 +33,30 @@
 	<div class="grow py-2">
 		<slot />
 	</div>
-	<input
-		bind:this={inputBox}
-		disabled={chatting}
-		bind:value
-		on:keydown={handleKeydown}
-		class={'input input-ghost input-bordered input-lg w-full'}
-	/>
+	<div class="form-control">
+		<div class="input-group input-group-lg">
+			<input
+				bind:this={inputBox}
+				disabled={chatting}
+				bind:value
+				on:keydown={handleKeydown}
+				class={'input w-full'}
+			/>
+			<button class="btn btn-square" on:click={chat}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+					/></svg
+				>
+			</button>
+		</div>
+	</div>
 </div>
